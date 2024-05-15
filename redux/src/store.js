95b1b5eed06a019592f344b0c68d83f91edee656
Yohannes Mesay <johnnymesay@gmail.com>
2024-05-15 16:33:03 +0300

    updated

--- redux/src/store.js
+++ redux/src/store.js
@@ -8,14 +8,19 @@ const initialState = {
 function reducer(state = initialState, action) {
   switch (action.type) {
     case "account/deposit":
-      return { ...state, balance: state.balance - action.payload };
+      return { ...state, balance: state.balance + action.payload };
     case "account/withdraw":
       return { ...state, balance: state.balance - action.payload };
 
     case "account/requestLoan":
       if (state.loan > 0) return state;
-      return { ...state, loan: action.payload };
-    case "account/payload":
+      return {
+        ...state,
+        loan: action.payload.amount,
+        loanPurpose: action.payload.purpose,
+        balance: state.balance + action.payload.amount,
+      };
+    case "account/payloan":
       return {
         ...state,
         loanPurpose: "",
@@ -25,3 +30,24 @@ function reducer(state = initialState, action) {
       return state;
   }
 }
+const store = createStore(reducer);
+function deposit(amount) {
+  return { type: "account/deposit", payload: amount };
+}
+function withdraw(amount) {
+  return { type: "account/withdraw", payload: amount };
+}
+function requestLoan(amount, purpose) {
+  return { type: "account/requestLoan", payload: { amount, purpose } };
+}
+function payLoan() {
+  return { type: "account/payloan" };
+}
+store.dispatch(deposit(50));
+console.log(store.getState());
+
+store.dispatch(requestLoan(20, "gambling"));
+
+console.log(store.getState());
+store.dispatch(payloan());
+console.log(store.getState());
