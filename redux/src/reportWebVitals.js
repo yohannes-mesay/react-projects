105ac81993b024e0c8dfcdd52bfdff1ddd413ab2
Yohannes Mesay <johnnymesay@gmail.com>
2024-06-04 22:36:32 +0300

    update

--- redux/src/reportWebVitals.js
+++ redux/src/reportWebVitals.js
@@ -3,12 +3,14 @@ const reportWebVitals = onPerfEntry => {
     import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
       getCLS(onPerfEntry);
       getFID(onPerfEntry);
+      
       getFCP(onPerfEntry);
       getLCP(onPerfEntry);
       getTTFB(onPerfEntry);
     });
   }//hello world
-  
+
+
 
 };
 
