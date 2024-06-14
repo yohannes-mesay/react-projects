771d0fca4aa704143271b6748a3fc5855eec7596
Yohannes Mesay <johnnymesay@gmail.com>
2024-06-14 14:50:59 +0300

    added

--- redux/src/App.js
+++ redux/src/App.js
@@ -7,6 +7,7 @@ import { useSelector } from "react-redux";
 function App() {
 //update here
 //also here
+//also here
   const fullName = useSelector((store) => store.customer.fullName);
   return (
     <div>
