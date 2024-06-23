7606ce868fbd8a1c8ea727b3c3a70df5dbd33bb3
Yohannes Mesay <johnnymesay@gmail.com>
2024-06-23 06:10:10 +0300

    add

--- fast-pizza-app/src/features/order/CreateOrder.jsx
+++ fast-pizza-app/src/features/order/CreateOrder.jsx
@@ -83,6 +83,7 @@ function CreateOrder() {
     </Form>
   );
 }
+
 export async function action({ request }) {
   const formData = await request.formData();
   const data = Object.fromEntries(formData);
